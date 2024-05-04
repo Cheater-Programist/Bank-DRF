2f5f1006ccdaead0bd1b1b98870f0273907678cd
Cheater-Programist <muhammadaminibaydillayev@gmail.com>
2024-05-04 19:20:37 +0600

    Last changes

--- bank_drf/settings.py
+++ bank_drf/settings.py
@@ -139,7 +139,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # STATIC_ROOT = BASE_DIR / 'static'
-STATIC_ROOT = os.path.join(BASE_DIR)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/ 'media'
